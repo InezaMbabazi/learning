cd223b9f94290eed2dc09111d9422ccd4266d0ec
InezaMbabazi <106543808+InezaMbabazi@users.noreply.github.com>
2024-10-25 13:19:31 +0200

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,7 +1,7 @@
 import requests
 
 # Replace with your Canvas API URL and token
-canvas_url = 'https://kepler.instructure.com'
+canvas_url = 'https://kepler.instructure.com/api/v1'
 api_token = '1941~tNNratnXzJzMM9N6KDmxV9XMC6rUtBHY2w2K7c299HkkHXGxtWEYWUQVkwch9CAH'  # Replace with your actual API token
 course_id = '2624'  # Course ID from the link you provided
 
